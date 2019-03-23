c694e8c58d5766ae380777494297036bf3548cb3
kter <me@kter.jp>
2019-03-23 18:32:32 +0900

    トランジションエフェクト v-on:click

--- script.js
+++ script.js
@@ -29,4 +29,16 @@ var app4 = new Vue({
         { text: 'Build something awesome' }
       ]
     }
-  });
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+});
