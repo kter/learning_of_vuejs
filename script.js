23592c3188c1e25fe04ed7820581aa77504d648a
kter <me@kter.jp>
2019-03-23 18:12:26 +0900

    修正

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(event) {
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
   }
-})
-})
+});
 
-document.addEventListener("DOMContentLoaded", function(event) {
 var app2 = new Vue({
     el: '#app-2',
     data: {
       message: 'You loaded this page on ' + new Date().toLocaleString()
     }
-  })
-})
+});
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+});
