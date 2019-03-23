ba53a1352be5cef8a84d1a27bbe989219e9bdd0d
kter <me@kter.jp>
2019-03-23 18:55:40 +0900

    component

--- script.js
+++ script.js
@@ -49,3 +49,20 @@ var app6 = new Vue({
       message: 'Hello Vue!'
     }
 });
+
+Vue.component('todo-item', {
+    // todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+    // このプロパティは todo と呼ばれます。
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+})
