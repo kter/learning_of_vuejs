7c25f299f0772bd196f054e79c85c253d944dc8a
kter <me@kter.jp>
2019-03-24 15:59:39 +0900

    算出メソッド vs メソッド

--- computed.js
+++ computed.js
@@ -14,3 +14,16 @@ var vm = new Vue({
 // console.log(vm.reversedMessage) // => 'olleH'
 // vm.message = 'Goodbye'
 // console.log(vm.reversedMessage) // => 'eybdooG'
+
+
+// 上記算出メソッドと同じ結果だが、算出メソッドは値が更新されるまで結果がキャッシュされるのに対して、下記のようなメソッドはキャッシュされず都度実行される
+var vm2 = new Vue({
+    el: '#example2',
+    data: {
+      message: 'Hello'
+    },
+    methods: {
+        reverseMessage: function () {
+          return this.message.split('').reverse().join('')
+        }
+}});
