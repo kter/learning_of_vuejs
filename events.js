0f96b3ddff1ed0ace7551e8077551a2eb30beef5
kter <me@kter.jp>
2019-03-26 23:36:50 +0900

    v-onには関数名を指定することもできる

--- events.js
+++ events.js
@@ -4,3 +4,24 @@ var example1 = new Vue({
         counter: 0
     }
 });
+
+var example2 = new Vue({
+    el: '#example-2',
+    data: {
+        name: 'Vue.js'
+    },
+    // `methods` オブジェクトの下にメソッドを定義する
+    methods: {
+        greet: function (event) {
+            // メソッド内の `this` は、 Vue インスタンスを参照します
+            alert('Hello ' + this.name + '!')
+            // `event` は、ネイティブ DOM イベントです
+            if (event) {
+                alert(event.target.tagName)
+            }
+        }
+    }
+});
+
+// JavaScript からメソッドを呼び出すこともできます
+example2.greet() // => 'Hello Vue.js!'
